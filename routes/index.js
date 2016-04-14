b3f94baf8bfb2086c4c566c929e119e28ce26807
zhanwei <leaf_city@163.com>
2016-04-14 16:38:42 +0800

    fix minify bug for {{}} tags

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+var webTile = '歪闹日志';
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Index' });
+	res.render('index', {
+		title: '首页 - ' + webTile,
+		pageNav: 'index'
+	});
 });
 
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About' });
+	res.render('about', {
+		title: '关于 - ' + webTile,
+		pageNav: 'about'
+	});
 });
 
 module.exports = router;
